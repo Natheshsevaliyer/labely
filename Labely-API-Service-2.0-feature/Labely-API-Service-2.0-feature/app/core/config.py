121ffcd77f12a03460da5cb3c397a9c01031a434
Natheshsevaliyer <natheshsevaliyer26@gmail.com>
2026-07-01 12:22:01 +0530

    litle change

--- Labely-API-Service-2.0-feature/Labely-API-Service-2.0-feature/app/core/config.py
+++ Labely-API-Service-2.0-feature/Labely-API-Service-2.0-feature/app/core/config.py
@@ -1,5 +1,8 @@
 # app/core/config.py - Add Redis settings
 import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parentS[2]
 from typing import Optional, Set
 from urllib.parse import quote_plus
 
@@ -99,7 +102,7 @@ class Settings(BaseSettings):
     # Security
     JWT_ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_HOURS: int = 8
-
+    
     model_config = ConfigDict(
         env_file=".env",
         env_file_encoding="utf-8",
